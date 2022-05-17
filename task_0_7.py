33ae2efd545311b23b0784e03c13627ade1e588f
Magidi822 <78993642+Magidi822@users.noreply.github.com>
2022-05-17 18:16:07 +0200

    Update task_0_7.py

--- task_0_7.py
+++ task_0_7.py
@@ -2,12 +2,12 @@ def celsius_to_fahrenheit(celsius):
     fahrenheit = 9/5 * celsius + 32
     return fahrenheit
 
-print(celsius_to_fahrenheit())
+print(celsius_to_fahrenheit(32))
 
 
 def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return celsius
 
-print(fahrenheit_to_celsius())
+print(fahrenheit_to_celsius(100))
 	
