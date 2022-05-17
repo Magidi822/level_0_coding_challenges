4de9064459f4b3ed56cfcc15b20dbb34a8bb2d84
Magidi822 <78993642+Magidi822@users.noreply.github.com>
2022-05-17 21:43:43 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -1,18 +1,17 @@
 def number_to_time_conversion(number):
-    hour = number//60
+    hour = number // 60
     minutes = number % 60
+    plural_hour = "hour,"
+    min_plural = "minute"
     if number == 0:
-        print(hour, "hours,", minutes, "minutes")
-    elif hour == 1 and minutes > 1:
-        print(hour, "hour,", minutes, "minutes")
-    elif hour > 1 and minutes == 1:
-        print(hour, "hours,", minutes, "minute")
-    elif hour == 1 and minutes == 0:
-        print(hour, "hour,", minutes, "minutes")
-    elif hour == 0 and minutes == 1:
-        print(hour, "hours,", minutes, "minute")
-    else:
-        print(hour, "hours,", minutes, "minutes")
+        plural_hour = "hours,"
+        min_plural = "minutes"
+    if hour > 1 or hour == 0:
+        plural_hour = "hours,"
+    if minutes > 1 or minutes == 0:
+        min_plural = "minutes"
 
-number_to_time_conversion()
+    print(hour, plural_hour, minutes, min_plural)
 
+
+number_to_time_conversion(61)
