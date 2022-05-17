a2980de9cafe1407756cb4e3b70daca50fec7882
Magidi822 <78993642+Magidi822@users.noreply.github.com>
2022-05-17 18:14:17 +0200

    Update task_0_5.py

--- task_0_5.py
+++ task_0_5.py
@@ -1,7 +1,8 @@
 def area_of_triangle(side1, side2, side3):
-    semiperimeter = side1 + side2 + side3
-    area = 1/2 * (semiperimeter)
+    perimeter = side1 + side2 + side3
+    smp = 1 / 2 * perimeter
+    area = (smp * (smp - side1) * (smp - side2) * (smp - side3)) ** 0.5
     return area
 
 
-print(area_of_triangle(5, 6, 7))
+print(area_of_triangle(6, 5, 5))
